846de626c929886c314c8b25c320ca12b2d1616e
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-10-25 22:03:24 +0700

    update: re-structed folder
    create login routes

--- src/app/(routes)/dashboard/_components/sidebar.tsx
+++ src/app/(routes)/dashboard/_components/sidebar.tsx
@@ -1,18 +1,65 @@
 import Link from 'next/link'
+import { HiOutlineUsers } from 'react-icons/hi'
+import { LuUserCircle2 } from 'react-icons/lu'
 import LABEL from '../label'
-const Sidebar: React.FC = () => {
+
+interface Iprop {
+  onClickNavbar: (navbar: string) => void
+  isActiveNav: string
+}
+const Sidebar = () => {
+  const CENTER = 'flex items-center gap-4 justify-start'
   return (
-    <div className="bg-white text-gray-800 h-screen flex flex-col shadow-lg">
-      <h2 className="text-xl font-bold p-4 border-b text-blue-500 border-gray-700 text-center">
+    <div className="bg-white text-gray-800 min-h-screen h-full flex flex-col shadow-lg">
+      <h2 className="text-xl font-bold p-4 mb-4 border-b text-blue-500 text-center">
         <Link href={'/dashboard'}>{LABEL.sys.appName}</Link>
       </h2>
 
-      <ul className="flex flex-col p-4 space-y-2 flex-grow">
-        <li>
-          <Link href={'/dashboard/users'}>{LABEL.user.label}</Link>
+      <ul className="flex flex-col gap-4 opacity-60 pe-4 pt-3 space-y-2 flex-grow">
+        <li className={`ps-8`}>
+          <Link
+            href={'/dashboard/users'}
+            className={`${CENTER}`}
+          >
+            <HiOutlineUsers />
+            {LABEL.user.label}
+          </Link>
         </li>
-        <li>
-          <Link href={'/dashboard/authors'}>tác giả</Link>
+        <li className={`ps-8`}>
+          <Link
+            href={'/dashboard/authors'}
+            className={`${CENTER}`}
+          >
+            <LuUserCircle2 />
+            tác giả
+          </Link>
+        </li>
+        <li className={`ps-8`}>
+          <Link
+            href={'/dashboard/authors'}
+            className={`${CENTER}`}
+          >
+            <LuUserCircle2 />
+            tác giả
+          </Link>
+        </li>
+        <li className={`ps-8`}>
+          <Link
+            href={'/dashboard/authors'}
+            className={`${CENTER}`}
+          >
+            <LuUserCircle2 />
+            tác giả
+          </Link>
+        </li>
+        <li className={`ps-8`}>
+          <Link
+            href={'/dashboard/authors'}
+            className={`${CENTER}`}
+          >
+            <LuUserCircle2 />
+            tác giả
+          </Link>
         </li>
       </ul>
     </div>
