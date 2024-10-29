7235a59a45ba595cc5c4f4900ab680f925a5dd12
MaiThanhTai204 <taithanhmai204@gmail.com>
2024-10-29 12:15:59 +0700

    29-10

---
+++ src/app/(routes)/_component/Pagination.tsx
@@ -0,0 +1,53 @@
+// components/Pagination.js
+{/* <i class="fa-solid fa-angle-left"></i> */}
+{/* <i class="fa-solid fa-angle-right"></i> */}
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+const Pagination = ({ currentPage, totalPages }) => {
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="inline-flex -space-x-px text-sm">
+        <li>
+          <Link
+            href={`?page=${currentPage - 1}`}
+            className={`flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+              currentPage === 1 && 'pointer-events-none opacity-50'
+            }`}
+          >
+             <FontAwesomeIcon icon={faAngleLeft} /> {/* Icon Next */}
+          </Link>
+        </li>
+        {pageNumbers.map((page) => (
+          <li key={page}>
+            <Link
+              href={`?page=${page}`}
+              className={`flex items-center justify-center px-3 h-8 leading-tight ${
+                page === currentPage
+                  ? 'text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+                  : 'text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+              }`}
+            >
+              {page}
+            </Link>
+          </li>
+        ))}
+        <li>
+          <Link
+            href={`?page=${currentPage + 1}`}
+            className={`flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+              currentPage === totalPages && 'pointer-events-none opacity-50'
+            }`}
+          >
+             <FontAwesomeIcon icon={faAngleRight} /> {/* Icon Next */}
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
