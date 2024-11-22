4b2ed161af8477ca47c88aaefc19ac848700a66a
vo02022004 <148637808+vo02022004@users.noreply.github.com>
2024-11-22 23:45:58 +0700

    update search story, storypage, detailstory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function HomePage() {
   const onPageChange = (page: number) => {
     setCurrentPage(page)
   }
-
+  // asjjsa
   useEffect(() => {
     fetchStories()
   }, [currentPage])
